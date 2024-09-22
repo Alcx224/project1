7df66338d9cae660c790927273446278fcf9a8fa
Jean Pierre <jalvarezidep@gmail.com>
2024-09-22 12:34:26 -0500

    reestructuracion del proyecto

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:project1/main.dart';
+import 'package:project1/ui/widgets/my_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
